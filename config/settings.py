23871bd7baf5900768ed096a5a8f1f84168e943c
Huente <chuentecura57@gmail.com>
2026-03-22 12:28:06 -0300

    Logo optimizado

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'trustmarket_db',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
