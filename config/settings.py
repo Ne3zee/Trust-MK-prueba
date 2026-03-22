8bcdc231c18debbbf0cb701785d96c446fe5e627
Huente <chuentecura57@gmail.com>
2026-03-21 22:55:52 -0300

    Cambio de paleta y diseño primera version

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'trustmarket_db',
-        'USER': 'trustmarket_user',
-        'PASSWORD': 'Trustmarket202',
+        'USER': 'root',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
